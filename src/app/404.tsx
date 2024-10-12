d0d4ab8e22259f7220977931e573fd2fe55805e9
Joshua Edo <joshua.edo01@gmail.com>
2024-10-12 13:00:40 +0100

    feat: :rocket: create 404 page

--- src/app/404.tsx
+++ src/app/404.tsx
@@ -1,7 +1,33 @@
+import { buttonVariants } from '@/components/common/button';
+import { cn } from '@/lib/utils';
+import Section from '@/components/common/section';
+
 interface NotFoundPageProps {}
 
 const NotFoundPage = ({}: NotFoundPageProps) => {
-  return <div>NotFoundPage</div>;
+  return (
+    <Section
+      id='not-found'
+      className='bg-darker text-white flex-center h-screen lg:h-screen'
+    >
+      <div className='bg-brand flex-center w-[80%] md:w-[70%] lg:w-[60%] h-[60svh] md:h-[50svh] rounded-3xl shadow lg:-mb-32'>
+        <div className='flex-col-center gap-2 lg:gap-4'>
+          <p className='text-lg lg:text-xl lowercase'>OOPs! Page not found.</p>
+          <div className='flex gap-2 lg:gap-4'>
+            <a
+              href={'/'}
+              className={cn(
+                'text-xs md:text-sm',
+                buttonVariants({ variant: 'outline' })
+              )}
+            >
+              Go back to homepage
+            </a>
+          </div>
+        </div>
+      </div>
+    </Section>
+  );
 };
 
 export default NotFoundPage;
